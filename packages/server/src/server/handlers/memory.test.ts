24d42428c9edaf2a642e4fe79beb4d4a5ab265f0
wardpeet <ward@coding-tech.com>
2026-05-22 10:49:25 +0200

    fixes

--- packages/server/src/server/handlers/memory.test.ts
+++ packages/server/src/server/handlers/memory.test.ts
@@ -1318,10 +1318,7 @@ describe('Memory Handlers', () => {
         filter: undefined,
       });
 
-      expect(result).toEqual({
-        ...mockResult,
-        uiMessages: mockResult.messages,
-      });
+      expect(result).toEqual(mockResult);
       expect(mockMemory.getThreadById).toHaveBeenCalledWith({ threadId: 'test-thread' });
       expect(mockMemory.recall).toHaveBeenCalledWith({
         threadId: 'test-thread',
